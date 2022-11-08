0214a84d6b4be002de4c01a544b5ff01d6963d0e
txete <72020520+txete@users.noreply.github.com>
2022-11-08 15:31:07 +0100

    eFP color fixing

--- app/models/efp.py
+++ app/models/efp.py
@@ -67,23 +67,19 @@ class efp:
             expression_t = self.tissues_tmm.loc[gene_name] if norm == 'tmm' else self.tissues_log2_tmm.loc[gene_name]
 
             ticks_t = np.unique([re.sub(r'(?is)-.+', '', col) for col in self.tissues_tmm.columns])
-            self.data = {
-                t: round(np.average([val for item, val in expression_t.items() if item.__contains__(t + '-')]), 2)
-                for t in ticks_t
-            }
+            self.data = {t: round(np.average([val for item, val in expression_t.items() if item.__contains__(t + '-')]), 2) for t in ticks_t}
 
             expression_s = self.symbiosis_tmm.loc[gene_name] if norm == 'tmm' else self.symbiosis_log2_tmm.loc[gene_name]
             ticks_s = np.unique([re.sub(r'(?is)-.+', '', col) for col in self.symbiosis_tmm.columns])
-            self.data.update({
-                t: round(np.average([val for item, val in expression_s.items() if item.__contains__(t + '-')]), 2)
-                for t in ticks_s
-            })
+            self.data.update({t: round(np.average([val for item, val in expression_s.items() if item.__contains__(t + '-')]), 2) for t in ticks_s})
 
-            bins = sorted(np.unique([i for i in self.data.values()]))
+            bins = sorted(np.unique([i for i in self.data.values() if i != 0]))
+            is_zero = [True for i in self.data.values() if i == 0]
 
             if norm == 'tmm':
                 cmap = ['rgb(255, 255, 255)'] + n_colors('rgb(255, 255, 0)', 'rgb(255, 0, 0)', len(bins), 'rgb')
             else:
+                bins = sorted(np.unique([i for i in self.data.values()]))
                 cmap = ['rgb(255, 255, 255)'] + n_colors('rgb(0, 0, 255)', 'rgb(255, 0, 0)', len(bins), 'rgb')
             cmap = [self.rgb2hex(rgb) for rgb in cmap]
             cmap_dict = {i: c for i, c in enumerate(cmap)}
@@ -91,7 +87,7 @@ class efp:
             colors = [cmap_dict.get(i) for i in np.digitize(sorted(self.data.values()), bins)]
             svg_colors = {e[0] + '_fill': colors[i] for i, e in enumerate(sorted(self.data.items(), key=lambda kv: (kv[1], kv[0])))}
 
-            self.fig = self.init_legend(colors, bins, norm)
+            self.fig = self.init_legend(colors, bins, norm, is_zero if norm == 'tmm' else [])
             return svg_colors
 
         self.fig = None
@@ -107,7 +103,7 @@ class efp:
         rgb = [int(float(e)) for e in rgb.split(',')]
         return '#%02x%02x%02x' % (rgb[0], rgb[1], rgb[2])
 
-    def init_legend(self, colors, bins, norm):
+    def init_legend(self, colors, bins, norm, is_zero):
         """
         Creates the eFP legend colormap.
         """
@@ -115,6 +111,9 @@ class efp:
         fig = go.Figure()
 
         aux = sorted(bins)
+        if is_zero and norm == 'tmm':
+            aux = [0] + aux
+
         colors = np.flip(np.unique(colors)) if norm == 'tmm' else np.unique(colors)
         for i, c in enumerate(colors):
             name = aux.pop(0)
