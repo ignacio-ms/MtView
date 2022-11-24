2cabfa214b455219a3e9b97a8f8eedd975b35102
txete <72020520+txete@users.noreply.github.com>
2022-11-24 20:41:42 +0100

    Tools information

--- app/static/js/ref_modal.js
+++ app/static/js/ref_modal.js
@@ -40,7 +40,10 @@ function open_modal() {
                         "In:62.9(2021) - pp:1494-1500<br>"+
                         "<a href='https://doi.org/10.1093/pcp/pcab110' style='font-style: italic' target='_blank'>Ref</a><br>"+
                         "<br>"+
-                        "Data based on RNA-seq experiments aged from 2013 to 2022, normalized in both tmm and log2_tmm normalization methods."+
+                        "Data based on RNA-seq experiments aged from 2013 to 2022, normalized in both tmm and log2_tmm normalization methods.<br>"+
+                        "You can select one or several experiments to be plotted at the same time and select boths normalization methods.<br>"+
+                        "By clicking and dragging inside the plot you can select a section to be zoomed in, double-click to unzoom it.<br>"+
+                        "You can also deplot any experiment section by clicking into its respective square at te right side of the plot."+
                         "<br>"+
                         "<div id='cc-license-div' style='position: absolute; bottom: 10px; right: 10px'>"+
                         "    <a href='https://creativecommons.org/licenses/by/4.0/' target='_blank'>"+
@@ -59,7 +62,11 @@ function open_modal() {
                         "<br>"+
                         "Noble Research Institute<br>"+
                         "Developed by The Zao Bioinformatics Lab<br><br>"+
-                        "Data downloaded in raw counts and self normalized in tmm and log2_tmm normalization methods using edgeR."+
+                        "Data downloaded in raw counts and self normalized in tmm and log2_tmm normalization methods using edgeR.<br>"+
+                        "If a large section of the eFP is uncolored means there is not tissue localization data available of them.<br>"+
+                        "You can select both normalization methods, each one is displayed with different colomaps.<br>"+
+                        "By hovering the mouse on any tissue, its expression level will be displayed, by hovering the mouse over the legend, " +
+                        "that color will be focused while the rest of them will turn white, usefull for emphatise all tissues with the same expression level.<br>"+
                         "<br>";
                 }
                 break;
@@ -84,7 +91,11 @@ function open_modal() {
                         "<a href='https://doi.org/10.1038/s41586-021-03819-2' style='font-style: italic' target='_blank'>Ref</a><br>"+
                         "<br>"+
                         "AI prediction method using DeepLearning techniques.<br>"+
-                        "Data downloaded from AlphafoldDB (.pdb) and displayed using 3DMol library."+
+                        "Data downloaded from AlphafoldDB (.pdb) and displayed using 3DMol library.<br>"+
+                        "You can move the molecule view by clicking and dragging with the mouse and zoom it with the mouse wheel.<br>"+
+                        "Clicking any section of the protein will display the 'sticks and balls' mode of the atoms forming that " +
+                        "aminoacid and its neighbours, click it again to unselec the section or chick another one to focus on it.<br>"+
+                        "Click and drag a PAE plot section to select a domain of the protein and color it in the 3d representation, double-click to unselect it.<br>"+
                         "<br>"+
                         "<div id='cc-license-div' style='position: absolute; bottom: 10px; right: 10px'>"+
                         "   <a href='https://creativecommons.org/licenses/by/4.0/' target='_blank'>"+
@@ -107,6 +118,8 @@ function open_modal() {
                         "In:47.D1(2019) - pp:D607-D613<br>"+
                         "<a href='https://doi.org/10.1093/nar/gky1131' style='font-style: italic' target='_blank'>Ref</a><br>"+
                         "<br>"+
+                        "Each colored path between proteins represent a evidence of its interaction, proteins without known interaction" +
+                        "may not be really interacting. As more interaction evicences a interaction have, the more reliable is the interaction.<br>"+
                         "<div id='cc-license-div' style='position: absolute; bottom: 10px; right: 10px'>"+
                         "    <a href='https://creativecommons.org/licenses/by/4.0/' target='_blank'>"+
                         "        <img src='../static/SVGs/ccby.svg' alt='ccvy'>"+
